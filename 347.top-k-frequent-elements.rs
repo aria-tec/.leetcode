ac92f12998b13e61dcb9d39e46a0fb24190012f0
Raditya <arias@192.168.1.7>
2026-05-19 13:28:11 +0700

    day3.1

---
+++ 347.top-k-frequent-elements.rs
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=347 lang=rust
+ *
+ * [347] Top K Frequent Elements
+ */
+
+// @lc code=start
+use std::collections::{HashMap};
+impl Solution {
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut counts = HashMap::new();
+
+        for &nm in &nums {
+            // Check entry n, if not present, insert 0, evaluation result + 1
+            // key:nm value;.or_insert(0) += 1;
+            *counts.entry(nm).or_insert(0) += 1;
+        }
+        // create an empty nested vector (for freq[cn][nm])
+        let mut freq: Vec<Vec<i32>> = vec![vec![]; nums.len() + 1];
+
+        for (nm, cn) in counts {
+            // vector just want usize
+            let i = cn as usize;
+            // reversed, key = value, value = key
+            freq[i].push(nm);
+        }
+
+        let mut result: Vec<i32> = Vec::new();
+
+        for i in (1..freq.len()).rev() {
+            for &nm in &freq[i] {
+                result.push(nm);
+
+                if result.len() == (k as usize) {
+                    return result;
+                }
+            }
+        }
+        result
+    }
+}
+// @lc code=end
